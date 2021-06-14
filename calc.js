7d5a4dcd4aaf8ce7d35c03e818fdee82e0df5f50
JasmineGuy <guyjasmine.c@gmail.com>
2021-06-14 15:21:39 -0500

    add solution

--- calc.js
+++ calc.js
@@ -8,13 +8,34 @@ const reader = readline.createInterface({
 
 
 reader.question("What would you like to calculate?", function(input){
+	console.log("input", input)
 	tokens = input.split(' ');
-	
+	console.log('TOKENS: ', tokens)
+
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
 
+
+if ( mathSymbol === "+") {
+	console.log('RESULT: ', num1 + num2)
+} 
+if ( mathSymbol === "-") {
+	console.log('RESULT: ', num1 - num2)
+}
+if (mathSymbol === "*") {
+	console.log('RESULT: ', num1 * num2)
+}
+if (mathSymbol === "/") {
+	console.log('RESULT: ', num1 / num2)
+}
+if (mathSymbol === ("sqrt")) {
+	console.log('RESULT: ', Math.sqrt(num1))
+}
 	// This line closes the connection to the command line interface.
 	reader.close()
 
